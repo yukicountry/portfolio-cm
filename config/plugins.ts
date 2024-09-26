0b3517cfae9d772a264373b294c106fa5b5e49bf
Yuki Takada <yuki.takada@ytakada.dev>
2024-09-27 00:23:51 +0900

    add pg, aws-s3 updater

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_BUCKET_NAME"),
+          },
+        },
+      },
+    },
+  },
+});
